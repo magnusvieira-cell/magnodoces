3aac61419a0d4e30f31b2709da3cefea47cafb17
magnusvieira-cell <magnusvieira@gmail.com>
2026-08-31 16:38:46 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,18 @@
-// Importações do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// Configuração do seu Firebase
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBZ9j9lf8Zo9vCq3_Sp7uqomTb3XKGVVwk",
   authDomain: "magnodoces-c7af7.firebaseapp.com",
   projectId: "magnodoces-c7af7",
-  storageBucket: "magnodoces-c7af7.firebasestorage.app",
+  storageBucket: "magnodoces-c7af7.appspot.com",
   messagingSenderId: "76553441552",
   appId: "1:76553441552:web:c17aff981c7ad34aaad595"
 };
 
-// Inicialização
-const app = initializeApp(firebaseConfig);
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Banco de dados Firestore
-export const db = getFirestore(app);
+// Firestore
+const db = firebase.firestore();
 
 // Autenticação
-export const auth = getAuth(app);
+const auth = firebase.auth();
