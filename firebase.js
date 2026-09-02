2646004ec8da6d0ec72558f648e20a6d51049664
magnusvieira-cell <magnusvieira@gmail.com>
2026-09-02 17:27:50 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ var firebaseConfig = {
   apiKey: "AIzaSyBZ9j9lf8Zo9vCq3_Sp7uqomTb3XKGVVwk",
   authDomain: "magnodoces-c7af7.firebaseapp.com",
   projectId: "magnodoces-c7af7",
-  storageBucket: "magnodoces-c7af7.firebasestorage.app",
+  storageBucket: "magnodoces-c7af7.appspot.com",
   messagingSenderId: "76553441552",
   appId: "1:76553441552:web:c17aff981c7ad34aaad595"
 };
